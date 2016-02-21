9101ddb83bf67541b6711c1ac102d4e9b22ee2ae
Tommy <tom.staszyszyn@gmail.com>
2016-02-21 14:21:52 +0000

    concat minified scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,25 @@ module.exports = function(grunt) {
           baseDir: './src'
         }
       }
-    }
+    },
+    concat: {
+      options: {
+        separator: ';\n',
+      },
+      dist: {
+        src: [
+          'src/bower_components/angular/angular.min.js',
+          'src/bower_components/angular-route/angular-route.min.js',
+          'src/bower_components/angular-resource/angular-resource.min.js',
+          'src/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+          'src/bower_components/angular-animate/angular-animate.min.js',
+          'src/bower_components/jquery/dist/jquery.min.js',
+          'src/bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'src/bower_components/wow/dist/wow.min.js'
+        ],
+        dest: 'src/built.js',
+      },
+    },
   });
 
   grunt.registerTask('default', ['browserSync']);
